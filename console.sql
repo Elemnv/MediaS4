00682f3ce38727f7c72bf45c42680a29ee0ae3bd
Elmnva <elenamineeva02@mail.ru>
2021-04-05 21:49:24 +0400

    Update console.sql

--- console.sql
+++ console.sql
@@ -1,29 +1,44 @@
-create table userss (
-    id serial primary key,
-    name varchar(255),
-    last_name varchar(255),
-    email varchar(255),
-    work_id int
-
+create  table users(
+ id serial primary key,
+ name varchar(255),
+ login varchar(255)
 );
 
 create table works(
-    id serial primary key,
-    name varchar(255),
-    status bool
+ id serial primary key,
+ name varchar(255),
+ status bool
 );
 
-create table timings(
-    id serial primary key ,
-    code varchar(255),
-    track int,
-    work_id int
+create  tabletimings(
+ id serial primary key ,
+ code varchar(255),
+ track int,
+ work_id int,
+ users_id int
 );
 
-insert into userss (name, last_name,email,work_id) values ('lena','mineeva','elnv@mail.com',1);
-insert into works (name, status) values ('учитель',true);
-insert into timings(code,track,work_id) values ('fff',1,1);
+alter table users add age int default 0;
 
-select * from timings;
-select * from userss;
-select * from works;
+
+insert into users (name, login,age)
+    values ('elena','logi',18);
+
+ altertable users
+ add music int default 0;
+
+ altertable users
+ drop column music;
+
+select * from users
+    where age>> 15;
+
+select name,login from users
+    where name = 'elel';
+
+update users
+    set age = age + 1;
+
+delete from users
+    where age < 18;
+select * from users;
